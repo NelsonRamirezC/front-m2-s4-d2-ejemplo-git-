56e90e7f712bfe8640aa2c9c3b2ad5a909badcf7
Nelson Ramirez <xhyskoax@gmail.com>
2024-02-28 12:05:08 -0300

    se crea sección suscribete usando tooltips y evento click al botón, además se agrega footer a la página

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
-$(function(){
-   
-    $("h1").on("mouseenter", function(){
+$(function () {
+    const tooltipTriggerList = document.querySelectorAll(
+        '[data-bs-toggle="tooltip"]'
+    );
+    const tooltipList = [...tooltipTriggerList].map(
+        (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+    );
+
+    $("h1").on("mouseenter", function () {
         $(this).addClass("destacado");
     });
 
-    $("h1").on("mouseleave", function(){
+    $("h1").on("mouseleave", function () {
         $(this).removeClass("destacado");
     });
+
+
+    $("#btn-suscribete").on("click", function(){
+        alert("Gracias por suscribirte a nuestra web.")
+    })
 });
